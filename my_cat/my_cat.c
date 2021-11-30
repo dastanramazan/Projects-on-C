79580dc60eb7251b56c95e14d5b3ff96fdb1406b
dastanramazan <50314411+dastanramazan@users.noreply.github.com>
2021-12-01 01:19:00 +0600

    Update my_cat.c

--- my_cat/my_cat.c
+++ my_cat/my_cat.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
 
+
+int main (int argc, char *argv[]) {
+
+    for(int i = 1; i < argc; i++) {
+        FILE *fp;
+        fp = fopen(argv[i], "r");
+
+        char s = getc(fp); 
+        while(s != EOF) {
+            printf("%c", s);
+            s = getc(fp);
+        }
+    }
+
+    return 0;
